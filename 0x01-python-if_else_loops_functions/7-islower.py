c669812e16a9dd609ab0fa3c19f1cbb85140f591
emmlard <emmlard@gmail.com>
2022-08-24 13:44:29 +0100

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) in range(97, 123):
-        return("{} is lower".format(c))
-    else:
-        return("{} is upper".format(c))
+    return(ord("a") <= ord(c) <= ord("z"))
