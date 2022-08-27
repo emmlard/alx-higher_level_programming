2caf4befdc0e2afe5d1bc8d291fd51b7ab7f001f
emmlard <emmlard@gmail.com>
2022-08-27 09:38:04 +0100

    Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx in range(len(my_list)):
+        my_new_list = my_list
+        my_new_list[idx] = element
+        return (my_new_list)
+    else:
+        return (my_list)
