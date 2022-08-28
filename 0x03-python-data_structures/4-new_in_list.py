889f84c62d7d06f345e41e25ec24c4a54064f660
emmlard <emmlard@gmail.com>
2022-08-28 08:48:01 +0100

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,4 @@ def replace_in_list(my_list, idx, element):
     my_new_list = list(my_list)    
     if idx in range(len(my_new_list)):
         my_new_list[idx] = element
-        return (my_new_list)
-    else:
-        return (my_list)
+    return (my_new_list)
