ba859d592aa8d0edf852de8d204050a0c0bde6a8
emmlard <emmlard@gmail.com>
2022-10-14 12:03:07 +0100

    0x09. Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string += ["Holberton"]
-    return(", ".join(string))
+def magic_string(holder={'counter': 0}):
+    holder['counter'] += 1
+    return (', '.join(['BestSchool'] * (holder['counter'])))
